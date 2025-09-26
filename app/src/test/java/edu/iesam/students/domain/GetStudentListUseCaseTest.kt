967b549c056c21158423c7ecda2906d09dd618d9
Diego González Díaz <diego2612004@gmail.com>
2025-09-26 16:58:31 +0200

    Empezar a crear test de caso de uso para obtener el listado de alumnos.

---
+++ app/src/test/java/edu/iesam/students/domain/GetStudentListUseCaseTest.kt
@@ -0,0 +1,13 @@
+package edu.iesam.students.domain
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GetStudentListUseCaseTest {
+
+    @Test
+    fun `when invoke then return student list`() {
+
+
+    }
+}
